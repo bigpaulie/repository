813de8fdc2745c99e0e30652a320151585e4913c
Paul Purcel <bigpaulie@users.noreply.github.com>
2019-03-16 14:13:13 +0200

    Fixes #1

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
         with(new PersonMigration())->up();
     }
 
-    protected function setUp():void
+    protected function setUp(): void
     {
         with(new PersonSeeder())->run();
     }
